98b055d787c0e2f9067bd49f1ea9eb75cb08946f
Megaladon <stanko.demon@gmail.com>
2022-06-23 00:39:10 +0300

    Corrected version

--- OtherWork1/Program.cs
+++ OtherWork1/Program.cs
@@ -5,33 +5,56 @@ namespace OtherWork1
     class Program
     {
         static void Main(string[] args)
-        {
+        {  
             string InputString(string str)
             {
                 Console.Write(str);
                 return Console.ReadLine();
             }
 
-            void Circle(string pas, string secret)
+            Console.WriteLine("Введите номер дополнительной задачи:");
+            Console.WriteLine("1: В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.");
+            Console.WriteLine("2: Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается.");
+            int task = Convert.ToInt32(InputString(""));
+            switch (task)
             {
-                int count = 1;
-                while (pas != "exit")
-                {
-                    if (count == 3)
+                case 1:
+                    string secretMessage = "Секретное сообщение";
+                    string secretPassword = "Пароль";
+                    string password, cycle;
+                    int count = 0;
+                    do
                     {
-                        Console.WriteLine(secret);
-                    }
-                    else
-                    {
+                        Console.Clear();
+                        if (count > 0)
+                        {
+                            Console.WriteLine("Пароль неверный!");
+                        }
+                        Console.WriteLine(secretMessage);
+                        password = InputString("Введите пароль: ");
                         count++;
+                        if (count == 3)
+                        {
+                            Console.WriteLine("Вы исчерпали попытки, попробуйте в другой раз");
+                            break;
+                        }
+                    } while (password != secretPassword);
+                    if (password == secretPassword)
+                    {
+                        Console.WriteLine("Пароль принят");
                     }
-                    pas = InputString("Введите пароль: ");
-                }
-            }
+                    break;
+                case 2:                   
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вы не пройдёте дальше, пока не введете exit");
+                        cycle = InputString("Введите строку: ");
+                    } while (cycle != "exit");
+                    Console.WriteLine("Вы справились");
 
-            //Задание 1. В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
-            string password = InputString("Введите пароль: "), secretString = "Секретное сообщение!";
-            Circle(password, secretString);
+                    break;
+            }
 
             Console.ReadKey();
         }
